094c42b90e306493ef6ee4b41742b291161ddb63
Brenda Rearden <brenda@rancher.com>
2020-07-14 15:06:16 -0700

    Add istioctl upgrade

--- scripts/create_istio_system.sh
+++ scripts/create_istio_system.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 istioctl version
-istioctl install -f /app/istio-base.yaml
-istioctl verify-install -f /app/istio-base.yaml
+if kubectl get pods --namespace=istio-system | grep istiod
+then
+    echo "Running istioctl upgrade"
+    istioctl upgrade -y  -f /app/istio-base.yaml
+    istioctl verify-install -f /app/istio-base.yaml
+else
+    echo "Running istioctl install"
+    istioctl install -y -f /app/istio-base.yaml
+    istioctl verify-install -f /app/istio-base.yaml
+fi 
+if [ ${APPLY_OVERLAY_CONFIG} = true ]
+then
+    echo "Overlay configuration detect - running istioctl install"
+    istioctl install -f /app/overlay-config.yaml
+fi
